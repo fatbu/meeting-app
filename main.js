253b10228ae1618ab8847a301c4e1e3eb7990aff
fatbu <nicholastcwoo@gmail.com>
2021-05-19 17:46:33 +0800

    1.5 iteration

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const dataFileName = './data/persistent.json'
 const fs = require('fs')
 const fileContents = fs.readFileSync(dataFileName, 'utf8')
 
-const data = JSON.parse(fileContents)
+var data = JSON.parse(fileContents)
 
 var storeData = function() {
     fs.writeFileSync(dataFileName, JSON.stringify(data), 'utf8')
@@ -24,18 +24,69 @@ http.listen(port, () => {
     console.log('listening on *:' + port)
 })
 
+function getuuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
 
+function calcTimeForEvent() {
+
+}
 
 io.on('connection', function(socket) {
+    let signedinas = false
     socket.on('signin', function(cred) {
         console.log(JSON.stringify(cred))
+
+        if (data.users[cred.username] && data.users[cred.username].password === cred.password) {
+            // successful sign in
+            socket.emit('signedin', data.users[cred.username])
+            signedinas = cred.username
+        } else {
+            // unsuccessful sign in
+            socket.emit('signinfail')
+        }
     })
 
     socket.on('register', function(cred) {
-        console.log(JSON.stringify(cred))
         data.users[cred.username] = {
             password: cred.password
         }
         storeData()
     })
+
+    socket.on('togglefree', function(free) {
+        if (!signedinas) return
+        console.log(free)
+        let frees = data.users[signedinas].frees
+        frees = frees || {}
+        frees[free.date] = frees[free.date] || new Array(48).fill(false)
+        frees[free.date][free.index] = !frees[free.date][free.index]
+        data.users[signedinas].frees = frees
+        storeData()
+    })
+
+
+    socket.on('addevent', function(event) {
+        console.log(event)
+        event.earliest = Date.parse(event.earliest)
+        event.latest = Date.parse(event.latest)
+
+        if (!data.users[signedinas].events) {
+            data.users[signedinas].events = []
+        }
+        if (!data.events) data.events = {}
+        let eventid = getuuid()
+        data.events[eventid] = event
+        data.users[signedinas].events.push(eventid)
+        for (let username of event.participants) {
+            data.users[username].events.push(eventid)
+        }
+        storeData()
+    })
+
+
 })
--- public/index.js
+++ public/index.js
@@ -10,14 +10,93 @@ var signin = function() {
     })
 }
 
+var signedin = false
+var signedinas = ''
 
+var userdata = {}
 
 var register = function() {
     var username = $('#username').val()
     var password = $('#password').val()
+
+    if (username.indexOf(' ') >= 0) {
+        alert('Username cannot contain spaces')
+        return
+    }
+
 
     socket.emit('register', {
         username: username,
         password: password
     })
 }
+
+var createevent = function() {
+    if (!signedin) return
+
+
+
+    let eventdesc = $('#description').val()
+    let eventdur = $('#duration').val()
+    let earliestdate = $('#earliest').val()
+    let latestdate = $('#latest').val()
+    let participants = $('#participants').val().split(' ')
+
+    socket.emit('addevent', {
+        desc: eventdesc,
+        duration: eventdur,
+        earliest: earliestdate,
+        latest: latestdate,
+        participants: participants,
+    })
+}
+
+
+var dateView = new Date()
+
+var updateFree = function(freeSlots) {
+    $('#free > td').each(function() {
+        if (freeSlots && freeSlots[$(this).index()]) {
+            $(this).css('background-color', 'green')
+        } else {
+            $(this).css('background-color', 'grey')
+        }
+    })
+}
+$(document).ready(function() {
+    $('tr#free td').click(function() {
+        this.style.backgroundColor = this.style.backgroundColor == 'green' ? 'grey' : 'green';
+        socket.emit('togglefree', {
+            date: dateView.toDateString(),
+            index: $(this).index()
+        })
+        //console.log($(this).index())
+    })
+
+    $('#currentday').text(dateView.toDateString())
+
+    $('#back').click(function() {
+        dateView.setDate(dateView.getDate() - 1)
+        $('#currentday').text(dateView.toDateString())
+        updateFree(userdata.frees[dateView.toDateString()])
+    })
+    $('#forward').click(function() {
+        dateView.setDate(dateView.getDate() + 1)
+        $('#currentday').text(dateView.toDateString())
+        updateFree(userdata.frees[dateView.toDateString()])
+    })
+})
+
+
+
+socket.on('signedin', function(user) {
+    signedin = true
+    signedinas = $('#username').val()
+    userdata = user
+    updateFree(userdata.frees[dateView.toDateString()])
+
+})
+
+socket.on('signinfail', function() {
+    console.log('sign in fail')
+})
