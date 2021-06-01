ab9f8616ba06b8f6071af790eff6964236093a08
fatbu <nicholastcwoo@gmail.com>
2021-06-01 10:08:44 +0800

    add 2 digit times

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ http.listen(port, () => {
     console.log('listening on *:' + port)
 })
 
+// creates a random id for events
 function getuuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random() * 16 | 0,
@@ -32,19 +33,19 @@ function getuuid() {
     });
 }
 
+// check if a user is free at a certain time
 function userFreeAtTime(username, time) {
     let user = data.users[username]
-    //console.log(new Date(time) + ' ' + time)
+    // iterate over all user free slots
     for (let datestr in user.frees) {
+        // parse date from epoch time
         let datetime = new Date(parseInt(datestr))
-        //console.log(datetime + ' ' + datestr)
         for (let i = 0; i < 48; i++) {
+            // iterate over all free slots for this day
             if (user.frees[datestr][i]) {
-                //console.log(datetime.getTime() + i * 1800000 + ' ' + time)
+                // calculate the current free slot time
                 let freeslot = datetime.getTime() + i * 1800000
-                // console.log('checking ' + new Date(freeslot) + ' vs ' + new Date(time))
-                // console.log(freeslot, time)
-                // console.log('difference ' + Math.abs(time - freeslot))
+                // check if it is equal to the provided time
                 if (datetime.getTime() + i * 1800000 == time) {
                     return true
                 }
@@ -55,29 +56,33 @@ function userFreeAtTime(username, time) {
 
 }
 
+// calculate and save the time for an event
+// ensure that an event is created only when all the participants are free
 function calcTimeForEvent(eventId) {
     let event = data.events[eventId]
     let earliestdate = new Date(event.earliest)
     earliestdate.setHours(0)
     let latestdate = new Date(event.latest)
+    // increment the latest date by 1, to allow setting an event time on the latest day itself
     latestdate.setDate(latestdate.getDate() + 1)
     let earliest = earliestdate.getTime()
     let latest = latestdate.getTime()
 
+    // event duration in milliseconds
     let eventDuration = parseInt(event.duration) * 60 * 1000
 
+    // iterate over all possible event times
     for (let i = earliest; i < latest; i += 1800000) {
+        // counter for the number of participants that are free during this particular timeslot
         let nFree = 0
         for (let participant of event.participants) {
-            // console.log(new Date(i))
-            // console.log(i)
-            // console.log(userFreeAtTime(participant, i))
-
             if (eventDuration <= 30) {
+                // if event fits in one time slot, check if the participant is free during that slot
                 if (userFreeAtTime(participant, i)) {
                     nFree++
                 }
             } else {
+                // if the event is longer than one time slot, check the time slots that follow it as well
                 let notFree = false
                 for (let t = i; t - i < eventDuration; t += 1800000) {
                     if (!userFreeAtTime(participant, t)) {
@@ -90,8 +95,10 @@ function calcTimeForEvent(eventId) {
                 }
             }
         }
-
+        // if all the participants are free during this timeslot
         if (nFree == event.participants.length) {
+            // save event to each user's data
+            // add to busy slots
             for (let participant of event.participants) {
                 data.users[participant].busy = data.users[participant].busy || []
 
@@ -107,68 +114,101 @@ function calcTimeForEvent(eventId) {
             }
             data.events[eventId].time = i
             storeData()
+            console.log('Time found for event ' + eventId)
             break
         }
     }
 }
 
 
-calcTimeForEvent('5f1df719-fdfe-4181-874b-95de4b5bf84f')
-
+// server event listeners
 io.on('connection', function(socket) {
+
     let signedinas = false
+    // user attempts to sign in
     socket.on('signin', function(cred) {
         console.log(JSON.stringify(cred))
 
         if (data.users[cred.username] && data.users[cred.username].password === cred.password) {
             // successful sign in
             let userdata = data.users[cred.username]
-            userdata.userEvents = {}
-            for (let event of userdata.events) {
-                userdata.userEvents[event] = data.events[event]
+            if (userdata.events) {
+                userdata.userEvents = {}
+                for (let event of userdata.events) {
+                    userdata.userEvents[event] = data.events[event]
+                }
             }
             socket.emit('signedin', userdata)
 
-
+            // save username for later
             signedinas = cred.username
         } else {
             // unsuccessful sign in
             socket.emit('signinfail')
         }
     })
-
+    // user registers
     socket.on('register', function(cred) {
         data.users[cred.username] = {
             password: cred.password
         }
+
         storeData()
+        socket.emit('registered')
     })
 
+    // since only the event ids are stored in the user objects,
+    // need to add the event objects into the user objects
+    // before sending it to the client
+    var updateUserData = function() {
+        let userdata = data.users[signedinas]
+        if (userdata && userdata.events) {
+            userdata.userEvents = {}
+            for (let event of userdata.events) {
+                userdata.userEvents[event] = data.events[event]
+            }
+        }
+        socket.emit('update', userdata)
+    }
+
+    // update the user data for the client
+    socket.on('update', function() {
+        updateUserData()
+    })
+
+    // toggle free slot
     socket.on('togglefree', function(free) {
         if (!signedinas) return
-        console.log(free)
         let frees = data.users[signedinas].frees
+        // initialise free slot objects if they do not exist
+        // by default the user is not free in a slot
         frees = frees || {}
         frees[free.date] = frees[free.date] || new Array(48).fill(false)
         frees[free.date][free.index] = !frees[free.date][free.index]
         data.users[signedinas].frees = frees
         storeData()
+        updateUserData()
     })
 
-
+    // add event to users and calculate time
     socket.on('addevent', function(event) {
         event.earliest = Date.parse(event.earliest)
         event.latest = Date.parse(event.latest)
+        // the event creator must be a participant in the event
         event.participants.push(signedinas)
         if (!data.users[signedinas].events) {
             data.users[signedinas].events = []
         }
+        // initialise events object if it does not exist
         if (!data.events) data.events = {}
+        // create random id for event
         let eventid = getuuid()
         data.events[eventid] = event
         data.users[signedinas].events.push(eventid)
+        // add event id to each of the participant objects
         for (let username of event.participants) {
             if (!username) continue
+            if (!data.users[username].events) data.users[username].events = []
             data.users[username].events.push(eventid)
         }
         storeData()
--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 var socket = io()
 
+// called when the user clicks the sign in button
 var signin = function() {
     var username = $('#username').val()
     var password = $('#password').val()
@@ -11,10 +12,12 @@ var signin = function() {
 }
 
 var signedin = false
-var signedinas = ''
+var signedinas = '' // username of signed in user
 
+// stores user information after they are signed in
 var userdata = {}
 
+// called when the user clicks the register button
 var register = function() {
     var username = $('#username').val()
     var password = $('#password').val()
@@ -31,11 +34,13 @@ var register = function() {
     })
 }
 
+// show the create event form and hide rest of page
 var showcreateevent = function() {
     $('#app').hide()
     $('#create').show()
 }
 
+// called when the create event form button is clicked
 var createevent = function() {
     if (!signedin) return
 
@@ -59,17 +64,14 @@ var createevent = function() {
     $('#app').show()
 }
 
-
+// the day that is currently being viewed
 var dateView = new Date()
 dateView.setHours(0)
 dateView.setMinutes(0)
 dateView.setSeconds(0)
 dateView.setMilliseconds(0)
 
-console.log(dateView)
-console.log(dateView.getTime())
-
-
+// display the user's free slots
 var updateFree = function(freeSlots) {
     $('#free > td').each(function() {
         if (freeSlots && freeSlots[$(this).index()]) {
@@ -79,12 +81,11 @@ var updateFree = function(freeSlots) {
         }
     })
 }
-
+// display the user's busy slots
 var updateBusy = function(busySlots) {
-    if (!busySlots) return
     $('#events > td').each(function() {
-
-        if (busySlots.includes(dateView.getTime() + $(this).index() * 1800000)) {
+        // check each busy slot and see if the user is busy at that time
+        if (busySlots && busySlots.includes(dateView.getTime() + $(this).index() * 1800000)) {
             $(this).css('background-color', 'blue')
         } else {
             $(this).css('background-color', 'transparent')
@@ -92,20 +93,46 @@ var updateBusy = function(busySlots) {
 
     })
 }
-
+// display the user's events on the currently viewed day
 var updateEvents = function(events) {
+
+    function pad2(number) {
+
+        return (number < 10 ? '0' : '') + number
+    }
+    $('#upcoming_events').empty()
     if (!events) return
     for (let eventid in events) {
         let event = events[eventid]
         if (!event.time) continue
         let eventdate = new Date(event.time)
         if (eventdate.getDate() == dateView.getDate()) {
-            $('#upcoming_events').append('<p>' + eventdate.getHours() + ':' + eventdate.getMinutes() + ' ' + event.desc + '</p>')
+            $('#upcoming_events').append('<p>' + pad2(eventdate.getHours()) + ':' + pad2(eventdate.getMinutes()) + ' ' + event.desc + '</p>')
         }
     }
 }
 
+// refreshes busy and free slots and the events displayed for the current day
+var updateAll = function() {
+    if (!userdata) return
+    if (userdata.frees) updateFree(userdata.frees[dateView.getTime()])
+    if (userdata.busy) updateBusy(userdata.busy)
+    if (userdata.userEvents) updateEvents(userdata.userEvents)
+}
+// when an update event is sent from the server, refresh what is being viewed
+socket.on('update', function(data) {
+    userdata = data
+    updateAll()
+})
+
+// update every second
+setInterval(function() {
+    socket.emit('update')
+}, 1000)
+
+// add event listeners
 $(document).ready(function() {
+    // when free slot is clicked
     $('tr#free td').click(function() {
         this.style.backgroundColor = this.style.backgroundColor == 'green' ? 'grey' : 'green';
         socket.emit('togglefree', {
@@ -115,35 +142,38 @@ $(document).ready(function() {
         //console.log($(this).index())
     })
 
+    // display the current day as a formatted string
     $('#currentday').text(dateView.toDateString())
 
+    // when back button is clicked
     $('#back').click(function() {
         dateView.setDate(dateView.getDate() - 1)
         $('#currentday').text(dateView.toDateString())
-        updateFree(userdata.frees[dateView.getTime()])
-        updateBusy(userdata.busy)
+        updateAll()
     })
+    // when forward button is clicked
     $('#forward').click(function() {
         dateView.setDate(dateView.getDate() + 1)
         $('#currentday').text(dateView.toDateString())
-        updateFree(userdata.frees[dateView.getTime()])
-        updateBusy(userdata.busy)
+        updateAll()
     })
 })
 
 
-
+// when received confirmation that signed in
 socket.on('signedin', function(user) {
     signedin = true
     signedinas = $('#username').val()
     userdata = user
-    updateFree(userdata.frees[dateView.getTime()])
-    updateBusy(userdata.busy)
-
+    updateAll()
     $('#app').show()
     $('#signin').hide()
 })
 
 socket.on('signinfail', function() {
-    console.log('sign in fail')
+    alert('Sign in fail')
+})
+
+socket.on('registered', function() {
+    alert('Registered')
 })
